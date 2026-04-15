e6206113d0e808fb44a3145123630eacdd65c083
Katherine Eaton <ktmeaton@gmail.com>
2026-04-15 16:09:43 -0600

    db: modify run_all_patches to not leak credentials to log

--- db/run_all_patches.pl
+++ db/run_all_patches.pl
@@ -2,17 +2,16 @@
 
 =head1 Usage
 
-Usage: ./run_all_patches.pl -u <dbuser> -p <dbpassword> -h <dbhost> -d <dbname> -e <editinguser> [-s <startfrom>] [--test]
+Usage: ./run_all_patches.pl -e <editinguser> [-s <startfrom>] [--test]
 
--u, --user=         database login username   
--p, --pass=         database login pasword    
--h, --host=         database host
--d, --db=           database name
 -e, --editinguser=  user to write as patch executor
 -s, --startfrom=0   start patches from folder # (Default: 0)
--t, --test          Do not make permanent changes.      
+-p, --patchpath=    directory containing patches # (Default: script directory)
+-n, --nodie         Do not die if a patch fails
+-t, --test          Do not make permanent changes.
 
-e.g. `./run_all_patches.pl -u postgres -p postgres -h localhost -d fixture -e janedoe -s 00085 -t`
+e.g. `./run_all_patches.pl -e janedoe -s 00085 -t`
+e.g. `./run_all_patches.pl -e admin -s 150`
 
 =cut
 
@@ -24,29 +23,25 @@ use Data::Dumper;
 use File::Basename qw(dirname);
 use Cwd qw(abs_path);
 
-my $dbuser;
-my $dbpass;
-my $host;
-my $db;
 my $editinguser;
 my $startfrom = 0;
 my $test;
+my $db_patch_path = dirname(abs_path($0));
+my $nodie;
 
 GetOptions(
-    "user=s"        => \$dbuser,
-    "pass=s"        => \$dbpass,
-    "host=s"        => \$host,
-    "db=s"          => \$db,
     "editinguser=s" => \$editinguser,
     "startfrom:i"   => \$startfrom,
-    "test"          => \$test
+    "test"          => \$test,
+    "patchpath=s"   => \$db_patch_path,
+    "nodie"         => \$nodie,
 );
 
-my $db_patch_path = dirname(abs_path($0));
 chdir($db_patch_path);
 
 my @folders = grep /[0-9]{5}/, (split "\n", `ls -d */`);
-my $cmd = "PGPASSWORD=$dbpass psql -h $host -U $dbuser -t -c \"select patch_name from Metadata.md_dbversion\" -d $db";
+
+my $cmd = "psql -h \$PGHOST -U \$PGUSER -d \$PGDATABASE -t -c \"select patch_name from Metadata.md_dbversion\"";
 my @installed = grep {!/^$/} map {s/^\s+|\s+$//gr} `$cmd`;
 
 for (my $i = 0; $i < (scalar @folders); $i++) {
@@ -54,6 +49,7 @@ for (my $i = 0; $i < (scalar @folders); $i++) {
         chdir($db_patch_path);
         chdir($folders[$i]);
         my @patches = grep {!($_ ~~ @installed)} map {s/.pm//r} (split "\n", `ls`);
+
         for (my $j = 0; $j < (scalar @patches); $j++) {
             my $patch = $patches[$j];
 
@@ -62,12 +58,16 @@ for (my $i = 0; $i < (scalar @folders); $i++) {
                 next;
             }
 
-            my $cmd = "echo -ne \"$dbuser\\n$dbpass\" | mx-run $patch -H $host -D $db -u $editinguser" . ($test ? ' -t' : '');
+            my $cmd = "echo -ne \"\$PGUSER\\n\$PGPASSWORD\" | mx-run $patch -H \$PGHOST -D \$PGDATABASE -u $editinguser" . ($test ? ' -t' : '');
             print STDERR $cmd . "\n";
             system("bash -c '$cmd'");
 
             if (($? >> 8) == 255) { #execution error
-                die "Failed executing patch: $patch";
+                if ($nodie) {
+                    print STDERR "Failed executing patch: $patch";
+                } else {
+                    die "Failed executing patch: $patch";
+                }
             }
 
             print STDERR "\n\n\n";
