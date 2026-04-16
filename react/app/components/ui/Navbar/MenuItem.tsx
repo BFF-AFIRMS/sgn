c55d01bf74fa7377369398be23ba54a8186196e2
Katherine Eaton <ktmeaton@gmail.com>
2026-04-16 15:19:50 -0600

    react: navbar menu items will do full page reload

--- react/app/components/ui/Navbar/MenuItem.tsx
+++ react/app/components/ui/Navbar/MenuItem.tsx
@@ -6,7 +6,7 @@ export function MenuItem(props) {
     <DropdownMenuItem
         className="p-0 pl-2 pr-2 hover:bg-mgray-300 hover:hover-shadow-text rounded-sm whitespace-nowrap text-sm"
     >
-        <Link to={`${props.link}`} className="w-full">{props.text}</Link>
+        <Link to={`${props.link}`} className="w-full" reloadDocument>{props.text}</Link>
     </DropdownMenuItem>
   )
 }
