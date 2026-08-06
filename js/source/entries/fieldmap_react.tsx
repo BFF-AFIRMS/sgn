dd88910534fbee6f09a52bed3b54d405109cbcf9
Seth Traverse <setraver@ualberta.ca>
2026-08-06 11:30:14 -0600

    fix(fieldmap): fill left half of north arrow compass needle with white

--- js/source/entries/fieldmap_react.tsx
+++ js/source/entries/fieldmap_react.tsx
@@ -1853,7 +1853,7 @@ const FieldMapContainer: React.FC<FieldMapContainerProps> = ({
                                             transition: 'transform 0.2s ease-out'
                                         }}
                                     >
-                                        <path style={{ fill: 'none', stroke: 'rgb(0, 0, 0)', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '7px' }} d="M 99.395 63.781 L 99.395 238.843 L 7.257 292.897 L 99.395 63.781 Z" />
+                                        <path style={{ fill: '#ffffff', stroke: 'rgb(0, 0, 0)', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '7px' }} d="M 99.395 63.781 L 99.395 238.843 L 7.257 292.897 L 99.395 63.781 Z" />
                                         <path style={{ stroke: 'rgb(0, 0, 0)', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '7px', transformBox: 'fill-box', transformOrigin: '50% 50%' }} d="M 191.623 292.345 L 191.623 117.283 L 99.485 63.229 L 191.623 292.345 Z" transform="matrix(-1, 0, 0, -1, -0.000015, 0.000014)" />
                                         <text style={{ fontFamily: 'Roboto, sans-serif', fontSize: '60px', fontWeight: 572, whiteSpace: 'pre' }} x="76.43" y="46.758">N</text>
                                     </svg>
