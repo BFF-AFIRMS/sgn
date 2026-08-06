84488013a015293a1b5b4c461fc2817ca98b8b73
Seth Traverse <setraver@ualberta.ca>
2026-08-06 13:21:16 -0600

    fix(fieldmap): north arrow clipping

--- js/source/entries/fieldmap_react.tsx
+++ js/source/entries/fieldmap_react.tsx
@@ -1841,21 +1841,23 @@ const FieldMapContainer: React.FC<FieldMapContainerProps> = ({
                             {!isTransposed && (
                                 <div
                                     className="tw:absolute tw:top-4 tw:right-4 tw:z-50 tw:flex tw:flex-col tw:items-center tw:pointer-events-none"
-                                    style={{ minWidth: '60px' }}
+                                    style={{ minWidth: '70px', minHeight: '70px' }}
                                 >
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 200 300"
+                                        viewBox="0 0 200 200"
                                         width="32"
-                                        height="48"
+                                        height="60"
+                                        overflow="visible"
                                         style={{
                                             transform: `rotate(${northArrowRotation}deg)`,
+                                            transformOrigin: 'center 70%',
                                             transition: 'transform 0.2s ease-out'
                                         }}
                                     >
                                         <path style={{ fill: '#ffffff', stroke: 'rgb(0, 0, 0)', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '7px' }} d="M 99.395 63.781 L 99.395 238.843 L 7.257 292.897 L 99.395 63.781 Z" />
                                         <path style={{ stroke: 'rgb(0, 0, 0)', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '7px', transformBox: 'fill-box', transformOrigin: '50% 50%' }} d="M 191.623 292.345 L 191.623 117.283 L 99.485 63.229 L 191.623 292.345 Z" transform="matrix(-1, 0, 0, -1, -0.000015, 0.000014)" />
-                                        <text style={{ fontFamily: 'Roboto, sans-serif', fontSize: '70px', fontWeight: 572, whiteSpace: 'pre', fill: '#000000', stroke: '#ffffff', strokeWidth: '4px' }} x="76.43" y="46.758">N</text>
+                                        <text style={{ fontFamily: 'Roboto, sans-serif', fontSize: '70px', fontWeight: 572, whiteSpace: 'pre', fill: '#000000', textShadow: '0 0 10px white', transform: `rotate(${-northArrowRotation}deg)`, transformBox: 'fill-box', transformOrigin: 'center' }} x="76.43" y="35">N</text>
                                     </svg>
                                 </div>
                             )}
