544c0b642fffafa4a89afe3272d05af7a5555380
Seth Traverse <setraver@ualberta.ca>
2026-08-12 09:58:04 -0600

    fix(fieldmap): add transition to north arrow N

--- js/source/include/fieldmap/components/NorthArrow.tsx
+++ js/source/include/fieldmap/components/NorthArrow.tsx
@@ -49,7 +49,7 @@ export const NorthArrow: React.FC<NorthArrowProps> = ({ }) => {
             >
                 <path style={{ fill: '#ffffff', stroke: 'rgb(0, 0, 0)', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '7px' }} d="M 99.395 63.781 L 99.395 238.843 L 7.257 292.897 L 99.395 63.781 Z" />
                 <path style={{ stroke: 'rgb(0, 0, 0)', strokeLinecap: 'round', strokeLinejoin: 'round', strokeWidth: '7px', transformBox: 'fill-box', transformOrigin: '50% 50%' }} d="M 191.623 292.345 L 191.623 117.283 L 99.485 63.229 L 191.623 292.345 Z" transform="matrix(-1, 0, 0, -1, -0.000015, 0.000014)" />
-                <text style={{ fontFamily: 'Roboto, sans-serif', fontSize: '70px', fontWeight: 572, whiteSpace: 'pre', fill: '#000000', transform: `rotate(${-northArrowRotation}deg)`, transformBox: 'fill-box', transformOrigin: 'center' }} x="76.43" y="35">N</text>
+                <text style={{ fontFamily: 'Roboto, sans-serif', fontSize: '70px', fontWeight: 572, whiteSpace: 'pre', fill: '#000000', transform: `rotate(${-northArrowRotation}deg)`, transformBox: 'fill-box', transformOrigin: 'center', transition: 'transform 0.2s ease-out' }} x="76.43" y="35">N</text>
             </svg>
         </div>
     );
