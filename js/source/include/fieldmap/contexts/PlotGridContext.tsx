a3d3d8230c6c3f71191921a271c5baf9fa754954
Seth Traverse <setraver@ualberta.ca>
2026-08-20 11:10:44 -0600

    fix(fieldmap): derive bounds directly from dimensions

--- js/source/include/fieldmap/contexts/PlotGridContext.tsx
+++ js/source/include/fieldmap/contexts/PlotGridContext.tsx
@@ -117,58 +117,15 @@ export const PlotGridProvider: React.FC<FieldMapContextProps> = ({ trialId, auth
     }, [plotList]);
 
     const bounds = useMemo(() => {
-        if (plotList.length === 0) {
-            return {
-                minCol: 1,
-                maxCol: dimensions.cols || 1,
-                minRow: 1,
-                maxRow: dimensions.rows || 1,
-                numRows: dimensions.rows || 1,
-                numCols: dimensions.cols || 1
-            };
-        }
-        let minCol = Infinity;
-        let minRow = Infinity;
-        let maxCol = -Infinity;
-        let maxRow = -Infinity;
-
-        plotList.forEach(p => {
-            const x = Number(p.observationUnitPosition.positionCoordinateX);
-            const y = Number(p.observationUnitPosition.positionCoordinateY);
-            if (!isNaN(x)) {
-                if (x < minCol) minCol = x;
-                if (x > maxCol) maxCol = x;
-            }
-            if (!isNaN(y)) {
-                if (y < minRow) minRow = y;
-                if (y > maxRow) maxRow = y;
-            }
-        });
-
-        if (minCol === Infinity) minCol = 1;
-        if (maxCol === -Infinity) maxCol = 1;
-        if (minRow === Infinity) minRow = 1;
-        if (maxRow === -Infinity) maxRow = 1;
-
-        let finalMaxCol = maxCol;
-        let finalMaxRow = maxRow;
-
-        if (dimensions.cols > (maxCol - minCol + 1)) {
-            finalMaxCol = minCol + dimensions.cols - 1;
-        }
-        if (dimensions.rows > (maxRow - minRow + 1)) {
-            finalMaxRow = minRow + dimensions.rows - 1;
-        }
-
         return {
-            minCol,
-            maxCol: finalMaxCol,
-            minRow,
-            maxRow: finalMaxRow,
-            numRows: finalMaxRow - minRow + 1,
-            numCols: finalMaxCol - minCol + 1
+            minCol: 1,
+            maxCol: dimensions.cols || 1,
+            minRow: 1,
+            maxRow: dimensions.rows || 1,
+            numRows: dimensions.rows || 1,
+            numCols: dimensions.cols || 1
         };
-    }, [plotList, dimensions]);
+    }, [dimensions]);
 
     /**
      * Computed bounds including borders
